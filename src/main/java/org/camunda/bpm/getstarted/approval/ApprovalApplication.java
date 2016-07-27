9169d77f82e913df5b26a71b6f08248795af74fe
KaySpannberg <kay.spannberg@th-brandenburg.de>
2016-07-27 15:12:31 +0200

    firstVersion
    firstVersion

---
+++ src/main/java/org/camunda/bpm/getstarted/approval/ApprovalApplication.java
@@ -0,0 +1,10 @@
+package org.camunda.bpm.getstarted.approval;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Approval App")
+
+public class ApprovalApplication extends ServletProcessApplication {
+  // empty implementation
+}
